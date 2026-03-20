621a3dfaf3a6127fd36c718fdbaea5e5dbc79d10
Yellow <jonathanstandley32@gmail.com>
2026-03-20 05:55:47 -0500

    update

--- GigaLearnCPP/src/public/GigaLearnCPP/Util/Timer.h
+++ GigaLearnCPP/src/public/GigaLearnCPP/Util/Timer.h
@@ -2,22 +2,18 @@
 #include "../Framework.h"
 
 namespace GGL {
-	struct Timer {
-		std::chrono::steady_clock::time_point startTime;
-
-		Timer() {
-			Reset();
-		}
-
-		// Returns elapsed time in seconds
-		double Elapsed() {
-			auto endTime = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed = endTime - startTime;
-			return elapsed.count();
-		}
-
-		void Reset() {
-			startTime = std::chrono::high_resolution_clock::now();
-		}
-	};
+    struct Timer {
+        std::chrono::high_resolution_clock::time_point startTime;
+        Timer() {
+            Reset();
+        }
+        double Elapsed() {
+            auto endTime = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> elapsed = endTime - startTime;
+            return elapsed.count();
+        }
+        void Reset() {
+            startTime = std::chrono::high_resolution_clock::now();
+        }
+    };
 }
