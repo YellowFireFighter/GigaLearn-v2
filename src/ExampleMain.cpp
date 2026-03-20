380ce98f1d368fa73e4df0a51dccffd8ddd24270
Yellow <jonathanstandley32@gmail.com>
2026-03-19 20:41:46 -0500

    initial commit

--- src/ExampleMain.cpp
+++ src/ExampleMain.cpp
@@ -1,166 +1,174 @@
 #include <GigaLearnCPP/Learner.h>
-
 #include <RLGymCPP/Rewards/CommonRewards.h>
 #include <RLGymCPP/Rewards/ZeroSumReward.h>
 #include <RLGymCPP/TerminalConditions/NoTouchCondition.h>
 #include <RLGymCPP/TerminalConditions/GoalScoreCondition.h>
 #include <RLGymCPP/OBSBuilders/DefaultObs.h>
-#include <RLGymCPP/OBSBuilders/AdvancedObs.h>
 #include <RLGymCPP/StateSetters/KickoffState.h>
 #include <RLGymCPP/StateSetters/RandomState.h>
 #include <RLGymCPP/ActionParsers/DefaultAction.h>
+#include <RLGymCPP/Rewards/Reward.h>
 
-using namespace GGL; // GigaLearn
-using namespace RLGC; // RLGymCPP
+using namespace GGL;
+using namespace RLGC;
 
-// Create the RLGymCPP environment for each of our games
+// =========================================
+// SpeedTowardBallReward
+// =========================================
+class SpeedTowardBallReward : public Reward {
+public:
+    float GetReward(const Player& player, const GameState& state, bool isFinal) override {
+        Vec posDiff = state.ball.pos - player.pos;
+        float dist = posDiff.Length();
+        if (dist < 1.f) return 0.f;
+        Vec dirToBall = posDiff / dist;
+        float speedToward = player.vel.Dot(dirToBall);
+        return RS_MAX(0.f, speedToward / CommonValues::CAR_MAX_SPEED);
+    }
+};
+
+// =========================================
+// VelocityBallToGoalOnTouchReward
+// =========================================
+class VelocityBallToGoalOnTouchReward : public Reward {
+public:
+    float GetReward(const Player& player, const GameState& state, bool isFinal) override {
+        if (!player.ballTouchedStep) return 0.f;
+        float goalY = (player.team == Team::ORANGE) ? -CommonValues::BACK_NET_Y : CommonValues::BACK_NET_Y;
+        Vec posDiff = Vec(0, goalY, 0) - state.ball.pos;
+        float dist = posDiff.Length();
+        if (dist < 1.f) return 0.f;
+        Vec dirToGoal = posDiff / dist;
+        float velToward = state.ball.vel.Dot(dirToGoal);
+        return RS_MAX(0.f, velToward / CommonValues::BALL_MAX_SPEED);
+    }
+};
+
+// =========================================
+// KickoffProximityReward
+// =========================================
+class KickoffProximityReward : public Reward {
+public:
+    float GetReward(const Player& player, const GameState& state, bool isFinal) override {
+        if (state.ball.vel.Length() > 100.f) return 0.f;
+
+        float playerDist = (player.pos - state.ball.pos).Length();
+        float closestOpp = 1e9f;
+
+        for (auto& p : state.players) {
+            if (p.team != player.team) {
+                float d = (p.pos - state.ball.pos).Length();
+                if (d < closestOpp) closestOpp = d;
+            }
+        }
+
+        if (closestOpp >= 1e9f) return 0.f;
+        return (playerDist < closestOpp) ? 1.f : -1.f;
+    }
+};
+
+// =========================================
+// RandomStateSetter (80% kickoff, 20% random)
+// =========================================
+class RandomStateSetter : public StateSetter {
+public:
+    KickoffState kickoff;
+    RandomState random;
+
+    RandomStateSetter() : kickoff(), random(true, true, false) {}
+
+    void ResetArena(Arena* arena) override {
+        if ((rand() % 10) < 8)
+            kickoff.ResetArena(arena);
+        else
+            random.ResetArena(arena);
+    }
+};
+
+// =========================================
+// Env Create Function
+// =========================================
 EnvCreateResult EnvCreateFunc(int index) {
-	// These are ok rewards that will produce a scoring bot in ~100m steps
-	std::vector<WeightedReward> rewards = {
+    std::vector<WeightedReward> rewards = {
+        { new FaceBallReward(),                  1.0f  },
+        { new SpeedTowardBallReward(),            5.0f  },
+        { new VelocityBallToGoalOnTouchReward(),  5.0f  },
+        { new StrongTouchReward(),                10.0f },
+        { new GoalReward(),                       500.0f},
+        { new KickoffProximityReward(),           3.0f  },
+        { new SaveBoostReward(),                  0.5f  },
+    };
 
-		// Movement
-		{ new AirReward(), 0.25f },
+    std::vector<TerminalCondition*> terminalConditions = {
+        new NoTouchCondition(30),
+        new GoalScoreCondition()
+    };
 
-		// Player-ball
-		{ new FaceBallReward(), 0.25f },
-		{ new VelocityPlayerToBallReward(), 4.f },
-		{ new StrongTouchReward(20, 100), 60 },
+    int playersPerTeam = 1;
+    auto arena = Arena::Create(GameMode::SOCCAR);
+    for (int i = 0; i < playersPerTeam; i++) {
+        arena->AddCar(Team::BLUE);
+        arena->AddCar(Team::ORANGE);
+    }
 
-		// Ball-goal
-		{ new ZeroSumReward(new VelocityBallToGoalReward(), 1), 2.0f },
+    EnvCreateResult result = {};
+    result.actionParser = new DefaultAction();
+    result.obsBuilder = new DefaultObs();
+    result.stateSetter = new RandomStateSetter();
+    result.terminalConditions = terminalConditions;
+    result.rewards = rewards;
+    result.arena = arena;
 
-		// Boost
-		{ new PickupBoostReward(), 10.f },
-		{ new SaveBoostReward(), 0.2f },
-
-		// Game events
-		{ new ZeroSumReward(new BumpReward(), 0.5f), 20 },
-		{ new ZeroSumReward(new DemoReward(), 0.5f), 80 },
-		{ new GoalReward(), 150 }
-	};
-
-	std::vector<TerminalCondition*> terminalConditions = {
-		new NoTouchCondition(10),
-		new GoalScoreCondition()
-	};
-
-	// Make the arena
-	int playersPerTeam = 1;
-	auto arena = Arena::Create(GameMode::SOCCAR);
-	for (int i = 0; i < playersPerTeam; i++) {
-		arena->AddCar(Team::BLUE);
-		arena->AddCar(Team::ORANGE);
-	}
-
-	EnvCreateResult result = {};
-	result.actionParser = new DefaultAction();
-	result.obsBuilder = new AdvancedObs();
-	result.stateSetter = new KickoffState();
-	result.terminalConditions = terminalConditions;
-	result.rewards = rewards;
-
-	result.arena = arena;
-
-	return result;
+    return result;
 }
 
-void StepCallback(Learner* learner, const std::vector<GameState>& states, Report& report) {
-	// To prevent expensive metrics from eating at performance, we will only run them on 1/4th of steps
-	// This doesn't really matter unless you have expensive metrics (which this example doesn't)
-	bool doExpensiveMetrics = (rand() % 4) == 0;
-
-	// Add our metrics
-	for (auto& state : states) {
-		if (doExpensiveMetrics) {
-			for (auto& player : state.players) {
-				report.AddAvg("Player/In Air Ratio", !player.isOnGround);
-				report.AddAvg("Player/Ball Touch Ratio", player.ballTouchedStep);
-				report.AddAvg("Player/Demoed Ratio", player.isDemoed);
-
-				report.AddAvg("Player/Speed", player.vel.Length());
-				Vec dirToBall = (state.ball.pos - player.pos).Normalized();
-				report.AddAvg("Player/Speed Towards Ball", RS_MAX(0, player.vel.Dot(dirToBall)));
-
-				report.AddAvg("Player/Boost", player.boost);
-
-				if (player.ballTouchedStep)
-					report.AddAvg("Player/Touch Height", state.ball.pos.z);
-			}
-		}
-
-		if (state.goalScored)
-			report.AddAvg("Game/Goal Speed", state.ball.vel.Length());
-	}
-}
-
+// =========================================
+// Main
+// =========================================
 int main(int argc, char* argv[]) {
-	// Initialize RocketSim with collision meshes
-	// Change this path to point to your meshes!
-	RocketSim::Init("C:\\Users\\admin\\source\\repos\\RLArenaCollisionDumper\\collision_meshes");
+    RocketSim::Init("collision_meshes");
 
-	// Make configuration for the learner
-	LearnerConfig cfg = {};
+    LearnerConfig cfg = {};
+    cfg.deviceType = LearnerDeviceType::GPU_CUDA;
+    cfg.tickSkip = 8;
+    cfg.actionDelay = cfg.tickSkip - 1;
+    cfg.numGames = 192;
+    cfg.randomSeed = 123;
 
-	cfg.deviceType = LearnerDeviceType::GPU_CUDA;
+    int tsPerItr = 300'000;
+    cfg.ppo.tsPerItr = tsPerItr;
+    cfg.ppo.batchSize = tsPerItr;
+    cfg.ppo.miniBatchSize = 100'000;
+    cfg.ppo.epochs = 2;
+    cfg.ppo.entropyScale = 0.01f;
+    cfg.ppo.gaeGamma = 0.99;
+    cfg.ppo.policyLR = 2e-4;
+    cfg.ppo.criticLR = 2e-4;
 
-	cfg.tickSkip = 8;
-	cfg.actionDelay = cfg.tickSkip - 1; // Normal value in other RLGym frameworks
+    cfg.ppo.sharedHead.layerSizes = {};
+    cfg.ppo.policy.layerSizes = { 1024, 1024, 1024, 1024, 1024, 512 };
+    cfg.ppo.critic.layerSizes = { 1024, 1024, 1024, 512 };
 
-	// Play around with this to see what the optimal is for your machine, more games will consume more RAM
-	cfg.numGames = 256;
+    auto optim = ModelOptimType::ADAM;
+    cfg.ppo.policy.optimType = optim;
+    cfg.ppo.critic.optimType = optim;
+    cfg.ppo.sharedHead.optimType = optim;
 
-	// Leave this empty to use a random seed each run
-	// The random seed can have a strong effect on the outcome of a run
-	cfg.randomSeed = 123;
+    auto activation = ModelActivationType::RELU;
+    cfg.ppo.policy.activationType = activation;
+    cfg.ppo.critic.activationType = activation;
+    cfg.ppo.sharedHead.activationType = activation;
 
-	int tsPerItr = 50'000;
-	cfg.ppo.tsPerItr = tsPerItr;
-	cfg.ppo.batchSize = tsPerItr;
-	cfg.ppo.miniBatchSize = 50'000; // Lower this if too much VRAM is being allocated
+    bool addLayerNorm = true;
+    cfg.ppo.policy.addLayerNorm = addLayerNorm;
+    cfg.ppo.critic.addLayerNorm = addLayerNorm;
+    cfg.ppo.sharedHead.addLayerNorm = addLayerNorm;
 
-	// Using 2 epochs seems pretty optimal when comparing time training to skill
-	// Perhaps 1 or 3 is better for you, test and find out!
-	cfg.ppo.epochs = 1;
+    cfg.sendMetrics = true;
+    cfg.renderMode = false;
 
-	// This scales differently than "ent_coef" in other frameworks
-	// This is the scale for normalized entropy, which means you won't have to change it if you add more actions
-	cfg.ppo.entropyScale = 0.035f;
+    Learner* learner = new Learner(EnvCreateFunc, cfg);
+    learner->Start();
 
-	// Rate of reward decay
-	// Starting low tends to work out
-	cfg.ppo.gaeGamma = 0.99;
-
-	// Good learning rate to start
-	cfg.ppo.policyLR = 1.5e-4;
-	cfg.ppo.criticLR = 1.5e-4;
-
-	cfg.ppo.sharedHead.layerSizes = { 256, 256 };
-	cfg.ppo.policy.layerSizes = { 256, 256, 256 };
-	cfg.ppo.critic.layerSizes = { 256, 256, 256 };
-
-	auto optim = ModelOptimType::ADAM;
-	cfg.ppo.policy.optimType = optim;
-	cfg.ppo.critic.optimType = optim;
-	cfg.ppo.sharedHead.optimType = optim;
-
-	auto activation = ModelActivationType::RELU;
-	cfg.ppo.policy.activationType = activation;
-	cfg.ppo.critic.activationType = activation;
-	cfg.ppo.sharedHead.activationType = activation;
-
-	bool addLayerNorm = true;
-	cfg.ppo.policy.addLayerNorm = addLayerNorm;
-	cfg.ppo.critic.addLayerNorm = addLayerNorm;
-	cfg.ppo.sharedHead.addLayerNorm = addLayerNorm;
-
-	cfg.sendMetrics = true; // Send metrics
-	cfg.renderMode = false; // Don't render
-
-	// Make the learner with the environment creation function and the config we just made
-	Learner* learner = new Learner(EnvCreateFunc, cfg, StepCallback);
-
-	// Start learning!
-	learner->Start();
-
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
